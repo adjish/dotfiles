b0ce0d1371179cdd087658338702f3415c0280fd
adjish <adjish3@gmail.com>
2025-02-18 18:04:39 +0100

    Update swaybar config

--- sway/status.sh
+++ sway/status.sh
@@ -1,3 +1,9 @@
 volume="$(wpctl get-volume @DEFAULT_AUDIO_SINK@)"
+
+case "$volume" in
+*'MUTED'*) muted='MUTED' ;;
+esac
+
+volume="${volume% \[MUTED*}"
 volume=$(bc <<<"${volume#* }*100")
-echo "${volume%.*}"% 🔊 $(date '+%H:%M %a %d/%m/%y')
+echo "${volume%.*}"% $muted "$(date '+%H:%M %a %d/%m/%y')"
