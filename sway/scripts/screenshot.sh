05add132a0723d0a5e5c98c6f835613af3557604
adjish <adjish3@gmail.com>
2025-03-15 23:51:04 +0100

    Update sway script

--- sway/scripts/screenshot.sh
+++ sway/scripts/screenshot.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 
-save_path="$HOME/Pictures/Screenshots/Screenshot $(date +%s).png"
+save_path="${HOME}/Pictures/Screenshots/Screenshot_$(date +%s).png"
+
+while [ -e "${save_path}" ]; do
+  save_path="${save_path%.*}"
+  save_path="${save_path#*_}"
+
+  case "${save_path}" in
+  *_*)
+    number="${save_path#*_}"
+    number=$((number + 1))
+    save_path="${save_path%_*}"
+    save_path="Screenshot_${save_path}_${number}.png"
+    ;;
+  *)
+    save_path="Screenshot_${save_path}_1.png"
+    ;;
+  esac
+
+done
 
 grim "${save_path}"
 
-name="$(echo | tofi -c ~/.config/tofi/dmenu --prompt-text 'Filename: ')"
+name="$(tofi -c ~/.config/tofi/dmenu --prompt-text 'Custom filename: ' </dev/null)"
 
-if [ -n "$name" ]; then
-  mv "${save_path}" "$(dirname "$save_path")/$name"
-  save_path="$(dirname "$save_path")/$name"
+if [ -n "${name}" ]; then
+  mv "${save_path}" "$(dirname "${save_path}")/${name}"
+  save_path="$(dirname "${save_path}")/${name}"
 fi
 
-[ "$(printf 'Yes\nNo' | tofi -c ~/.config/tofi/dmenu --prompt-text 'Copy screenshot in clipboard? ')" = 'Yes' ] && wl-copy <"${save_path}"
+answer="$(printf 'No\nYes' | tofi -c ~/.config/tofi/dmenu --prompt-text 'Copy screenshot to clipboard? ')"
+
+if [ "${answer}" = 'Yes' ]; then
+  wl-copy <"${save_path}"
+fi
