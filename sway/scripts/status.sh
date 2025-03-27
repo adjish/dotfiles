39c2f8fb01dcd752a82eb4eebb235ccd6f66003c
adjish <adjish3@gmail.com>
2025-03-27 22:32:14 +0100

    Update sway script

--- sway/scripts/status.sh
+++ sway/scripts/status.sh
@@ -11,4 +11,4 @@ volume=$(echo "${volume#* }" | tr -d .)
 volume="${volume#0}"
 volume="${volume#0}"
 
-echo "${volume%.*}% $muted| $(date '+%H:%M %a %d/%m/%y')"
+echo "$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')" \| "${volume%.*}% $muted| $(date '+%H:%M %a %d/%m/%y')"
