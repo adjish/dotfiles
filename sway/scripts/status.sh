f31e6dda583bf249bafb2556dee5c3073e5c75cf
adjish <adjish3@gmail.com>
2025-03-28 23:05:50 +0100

    Update sway script

--- sway/scripts/status.sh
+++ sway/scripts/status.sh
@@ -2,13 +2,22 @@
 
 volume="$(wpctl get-volume @DEFAULT_SINK@)"
 
-case "$volume" in
-*'MUTED'*) muted='MUTED ' ;;
+case "${volume}" in
+*'MUTED'*) muted='true' ;;
+*) ;;
 esac
 
 volume="${volume% \[MUTED*}"
 volume=$(echo "${volume#* }" | tr -d .)
 volume="${volume#0}"
 volume="${volume#0}"
+volume="${volume%.*}%"
 
-echo "$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')" \| "${volume%.*}% $muted| $(date '+%H:%M %a %d/%m/%y')"
+if [ "${muted}" = 'true' ]; then
+  volume="${volume} MUTED"
+fi
+
+datetime="$(date '+%H:%M %a %d/%m/%y')"
+network="$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')"
+
+echo "${network}" \| "${volume}" \| "${datetime}"
