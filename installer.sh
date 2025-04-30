4b23c9314f612a86d786fa0cebe07f4b6210240b
adjish <adjish3@gmail.com>
2025-04-30 23:00:08 +0200

    Update installer

--- installer.sh
+++ installer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 for directory in conky foot mako mpd sway tofi zsh; do
-  command -v "${directory}" && cp -ri "${directory}" ~/.config/
+  command -v "${directory}" >/dev/null 2>&1 && cp -ri "${directory}" ~/.config/
 done
+
+command -v pacman >/dev/null 2>&1 && sudo cp -i pacman/pacman.conf /etc/pacman.conf && sudo cp -ri pacman/pacman.d /etc/
