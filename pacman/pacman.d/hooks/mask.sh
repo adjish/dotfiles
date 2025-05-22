3ee6f867412a8c271d93eecd6af58b5bd4180d54
adjish <adjish3@gmail.com>
2025-05-22 22:24:57 +0200

    Update pacman config

---
+++ pacman/pacman.d/hooks/mask.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/bin/systemctl --runtime mask halt.target poweroff.target reboot.target kexec.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target sleep.target >/dev/null 2>&1
