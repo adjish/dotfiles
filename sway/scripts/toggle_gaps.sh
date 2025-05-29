b48f928d075ce68da44510676a918dc70d8ceda4
adjish <adjish3@gmail.com>
2025-05-29 22:35:08 +0200

    Update sway config

---
+++ sway/scripts/toggle_gaps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+readonly STATE_FILE='/tmp/sway_gaps_enabled'
+
+state=1
+
+if [ -s "${STATE_FILE}" ]; then
+    read -r state < "${STATE_FILE}"
+fi
+
+if [ "${state}" -eq 1 ]; then
+    swaymsg gaps inner all set 0
+else
+    swaymsg gaps inner all set 10
+fi
+
+echo $((1 - state)) > "${STATE_FILE}"
