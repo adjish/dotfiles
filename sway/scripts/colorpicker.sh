d7d0a4c4361cd2ef3e309152d75a9e37bca30fb5
adjish <adjish3@gmail.com>
2026-01-25 21:24:33 +0100

    Improve user interface

--- sway/scripts/colorpicker.sh
+++ sway/scripts/colorpicker.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-notify-send "$(grim -g "$(slurp -p -b '#00000000')" -t ppm - | magick - txt: | awk 'NR == 2 {print $3}')"
+color="$(grim -g "$(slurp -p -b '#00000000')" -t ppm - | magick - txt: | awk 'NR == 2 {print $3}')"
+
+notify-send "Color" "${color}"
+
+answer="$(printf 'No\nYes' | tofi -c ~/.config/tofi/dmenu --prompt-text 'Copy color to clipboard? ')"
+
+if [ "${answer}" = 'Yes' ]; then
+  printf "%s" "${color}" | wl-copy
+fi
