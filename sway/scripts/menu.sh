521601a84bdc089976fabd4681abbd465e49f0dc
adjish <adjish3@gmail.com>
2026-01-31 23:24:04 +0100

    Improve user interface

--- sway/scripts/menu.sh
+++ sway/scripts/menu.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
 
+confirm()
+{
+  answer="$(printf 'No\nYes' | tofi -c ~/.config/tofi/dmenu --prompt-text "Confirm ${1}? ")"
+
+  if [ "${answer}" != 'Yes' ]; then
+    exit
+  fi
+}
+
 case $(printf "Shutdown\nPoweroff\nReboot\nSuspend" | tofi -c ~/.config/tofi/dmenu) in
-"Shutdown")
-  exec shutdown now
-  ;;
-"Poweroff")
-  exec poweroff
-  ;;
-"Reboot")
-  exec reboot
-  ;;
-"Suspend")
-  exec systemctl suspend
-  ;;
-*)
-  exit 1
-  ;;
+  "Shutdown")
+    confirm shutdown
+    exec shutdown now
+    ;;
+  "Poweroff")
+    confirm poweroff
+    exec poweroff
+    ;;
+  "Reboot")
+    confirm reboot
+    exec reboot
+    ;;
+  "Suspend")
+    confirm suspend
+    exec systemctl suspend
+    ;;
+  *)
+    exit 1
+    ;;
 esac
